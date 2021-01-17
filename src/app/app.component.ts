635d21aa42da5fc3f5a05a7eac3ab4cef35ae19d
Jeff Peralta <jeff@tsscore.com>
2021-01-17 08:06:59 +0800

    Connect to firebase + add posts + get posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,44 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(
+      "https://ng-test-fe501-default-rtdb.firebaseio.com/posts.json"
+    , postData
+    ).subscribe(response => {
+      console.log(response)
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get("https://ng-test-fe501-default-rtdb.firebaseio.com/posts.json")
+      .pipe(
+        map(data => { 
+          const postArray = [];
+          for(const key in data) {
+            if(data.hasOwnProperty(key)) {
+              postArray.push({ ...data[key], id: key })
+            }
+          }
+          return postArray;
+         })
+      )
+      .subscribe(response => {
+        console.log(response)
+      })
   }
 }
